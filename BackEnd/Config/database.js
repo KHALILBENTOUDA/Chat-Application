084f6aa09b62a7bc80803791357485c703e1e8ef
KHALILBENTOUDA <126770033+KHALILBENTOUDA@users.noreply.github.com>
2024-02-06 13:59:44 +0100

    Update database.js

--- BackEnd/Config/database.js
+++ BackEnd/Config/database.js
@@ -1,22 +1,20 @@
-const mysql=require('mysql2')
-const db=mysql.createConnection({
-    host:  process.env.DB_HOST,
-    user:  process.env.DB_USER,
-     password: process.env.DB_PASS,
-    port:  process.env.DB_PORT,
-     database: process.env.DB_NAME,
-      
-     charset: 'utf8mb4',
-  })
-  db.connect((err)=>{
-      if (err) {
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    charset: 'utf8mb4'
+});
+
+db.promise().connect()
+    .then(() => {
+        console.log('Connected to MySQL');
+    })
+    .catch((err) => {
         console.error('Error connecting to MySQL:', err);
-        return;
-      }
-  })
-module.exports = {db}
+    });
 
-
-
-
-
+module.exports = { db };
